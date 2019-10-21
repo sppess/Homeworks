f0e1232af438a7c01fa4a0838612a1e370f09516
sppess <55408873+sppess@users.noreply.github.com>
2019-10-21 20:35:04 +0300

    hw-3-task-3 was added

---
+++ homework-3/hw-3-task-3.py
@@ -0,0 +1,14 @@
+# num1 = int(input("Enter num1: "))
+# str_num1 = str(num1)
+# rev_str_num1 = str_num1[::-1]
+# if rev_str_num1 == str_num1:
+#     print(True)
+# else:
+#     print(False)
+
+num1 = input("Enter num1: ")
+rev_num1 = num1[::-1]
+if rev_num1 == num1:
+    print(True)
+else:
+    print(False)
