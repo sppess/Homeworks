34b6a4617bb13434fcfb3ae5de9d71f4867131b5
sppess <55408873+sppess@users.noreply.github.com>
2019-10-21 20:29:00 +0300

    hw-2-task-1 was added

---
+++ homework-2/hw-2-task-1.py
@@ -0,0 +1,9 @@
+a = 23.25
+b = 3
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Division: ", a / b)
+print("Multiplication: ", a * b)
+print("Exponent (Power): ", a ** b)
+print("Modulus: ", a % b)
+print("Floor Division: ", a // b)
