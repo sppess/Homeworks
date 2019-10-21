df2be20a35fcf9a1aa538394029a6e7b10ffc895
sppess <55408873+sppess@users.noreply.github.com>
2019-10-21 20:32:09 +0300

    hw-2-task-2 was added

---
+++ homework-2/hw-2-task-2.py
@@ -0,0 +1,6 @@
+name = 'Tanya'
+day = 'Friday'
+print("Good day", name, "!", day, 'is a perfect day to learn some python.')
+first_name = 'Tanya'
+last_name = 'Cherkes'
+print ("Good day", first_name + " " + last_name, "!", day, 'is a perfect day to learn some python.')
