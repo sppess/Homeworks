ebe0feee67f59ec336704b68de6f32ec2f42fb02
sppess <55408873+sppess@users.noreply.github.com>
2019-10-29 02:16:10 +0200

    hw-15-task-3 was added

---
+++ homework-15/hw-15-task-3.py
@@ -0,0 +1,76 @@
+# Add a new method to our Worker-Boss program to the Boss class.
+# This method is called (dump_workers). It must take all workers from
+# workers list and output them into a .csv file (just the way we did it)
+#
+# Extra point for doing it using built-in csv library
+# Extra point for doing it using 3rd party library pandas
+
+import csv
+from abc import ABC
+
+
+class Person(ABC):
+    def __init__(self, id_, name, company):
+        self.id = id_
+        self.name = name
+        self.company = company
+
+
+class Boss(Person):
+    def __init__(self, id_, name, company):
+        super(Boss, self).__init__(id_, name, company)
+        self.workers = []
+
+    def __repr__(self):
+        return f"Boss-{self.id}: {self.name}, {self.company}"
+
+    def dump_workers(self):
+        with open('files/new_workers_file.csv', 'w') as file:
+            file.write('id_,name,company,boss_id\n')
+            for index, worker in enumerate(workers):
+                file.write(f"{workers[index].id},"
+                           f"{workers[index].name},"
+                           f"{workers[index].company},"
+                           f"{workers[index]._boss.id}\n")
+
+
+class Worker(Person):
+    def __init__(self, id_, name, company, boss):
+        super(Worker, self).__init__(id_, name, company)
+        self._boss = boss
+        self._boss.workers.append(self)
+
+    def __repr__(self):
+        return f"[Worker-{self.id}]: {self.name}, {self.company}, " \
+                f"my boss is {self._boss.name}"
+
+    @property
+    def boss(self):
+        return self._boss
+
+    @boss.setter
+    def boss(self, new_boss):
+        if isinstance(new_boss, Boss):
+            self._boss.workers.remove(self)
+            self._boss = new_boss
+            self._boss.workers.append(self)
+        else:
+            print("This person is not a Boss")
+
+
+bosses = []
+workers = []
+
+with open('bosses.csv', 'r') as file:
+    readfile = file.readlines()
+    for boss in readfile[1:]:
+        id_, name, company = boss.split(',')
+        bosses.append(Boss(id_, name, company))
+
+with open('workers.csv', 'r') as file1:
+    for worker in file1.readlines()[1:]:
+        id_, name, company, boss_id = worker.split(',')
+        workers.append(Worker(id_, name, company, bosses[int(boss_id)]))
+
+print(workers)
+bosses[0].dump_workers()
