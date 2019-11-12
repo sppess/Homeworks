addad81c46dbe2c644708e0be36c5e01a9ad978f
sppess <55408873+sppess@users.noreply.github.com>
2019-11-12 18:54:34 +0200

    hw-23-task-1 was recreated

--- homework-23/hw-23-task-1.py
+++ homework-23/hw-23-task-1.py
@@ -1,9 +1,10 @@
-# Write a function copy_string which takes a string and recursively, character
-# by character creates a copy of it.
+# Write a function is_palindrome which takes a string and returns boolean
+# whether the string is a palindrome or not
 
 
-def copy_string_v1(string: str, new_string='') -> str:
-    if len(string) == 0:
-        return new_string
-    new_string += string[0]
-    return copy_string_v1(string[1:], new_string)
+def is_palindrome(word: str) -> bool:
+    if word[0] != word[-1]:
+        return False
+    if len(word) <= 2:
+        return True
+    return is_palindrome(word[1:-1])
